ff942d9573d1b285f20f46c5085e0fa7328af5f1
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-21 15:05:27 +0200

    Initial commit

---
+++ spmf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
