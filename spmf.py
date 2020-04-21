55639a81ea4e66273bb83e8a624f4f85238ab725
Lorenz Leitner <lorenz.leitner@student.tugraz.at>
2020-04-21 19:09:14 +0200

    To pandas dataframe

--- spmf.py
+++ spmf.py
@@ -11,6 +11,7 @@ __author__ = "Lorenz Leitner"
 __version__ = "0.1.0"
 __license__ = "GNU GPL v3.0"
 
+import pandas as pd
 import os
 import subprocess
 
@@ -25,6 +26,7 @@ class Spmf:
         self.input_ = input_filename
         self.output_ = output_filename
         self.arguments_ = [str(a) for a in arguments]
+        self.patterns_ = []
 
     def run(self):
         subprocess_arguments = [
@@ -52,4 +54,17 @@ class Spmf:
             # http://data-mining.philippe-fournier-viger.com/introduction-to-sequential-rule-mining/#comment-4114
             patterns.append(line.split(" -1 "))
 
+        self.patterns_ = patterns
         return patterns
+
+    def to_pandas_dataframe(self):
+        patterns_dict_list = []
+        for pattern_sup in self.patterns_:
+            pattern = pattern_sup[:-1]
+            sup = pattern_sup[-1:][0]
+            sup = sup[len(sup) - 1]
+
+            patterns_dict_list.append({'pattern': pattern, 'sup': sup})
+
+        df = pd.DataFrame(patterns_dict_list)
+        return df
